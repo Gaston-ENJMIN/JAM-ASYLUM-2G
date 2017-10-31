dd62bb4b42c8a3be3707bc825976f00cfa1af07c
Stonga <gaston.robert.auditeur@lecnam.net>
2017-10-31 14:49:43 +0100

    First

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    void Update()
+    {
+        LookMouseCursor();
+    }
+
+    void LookMouseCursor()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# JAM-ASYLUM-2G
-Jam asylum des 2G 2017
+3D Top Down
