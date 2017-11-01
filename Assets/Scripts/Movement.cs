fe49edbe2d6763c759d09d501cbcef2a9c31525a
Stonga <gaston.robert.auditeur@lecnam.net>
2017-11-01 12:51:46 +0100

    TEST_Normalement y a tout
    TEST entre PC portable et fixe pour voir si tout marche correctement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,20 +3,36 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
+    public Light lampe;
 
+    // COURAGE TU VAS COMPRENDRE EN REFLECHISSANT BIEN !
     void Update()
     {
+        // J'appelle la fonction LookMouseCursor définie plus loin
         LookMouseCursor();
+        LampeTorche();
     }
 
     void LookMouseCursor()
     {
+        // Je définis le fait que hit sera la variable lorsque le rayon va toucher une surface / Je trace le rayon
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        // Si le rayon touche quelque chose dans une range de 100
         if (Physics.Raycast(ray, out hit, 100))
         {
+            // L'objet regarde vers les coordonnées de hit
             transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+    }
+
+    void LampeTorche()
+    {   
+        // Je switch l'état ON / OFF en appuyant sur F
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            lampe.enabled = !lampe.enabled;
         }
     }
 }
---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+    float Z; float X;
+    public float speed = 0.0f;
+    float i;
+
+    public float accelUp = 1.0f;
+    public float accelDown = 1.0f;
+    public float speedWalk = 1.0f;
+    public float speedRun = 3.0f;
+
+    // Use this for initialization
+    void Start() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetButton("run") && (Input.GetAxis("Horizontal")!= 0 || Input.GetAxis("Vertical")!=0))
+            speed += Time.deltaTime * accelUp;
+        else
+            speed -= Time.deltaTime * accelDown;
+
+        speed = Mathf.Clamp(speed, speedWalk, speedRun );
+              
+
+        
+         X = Input.GetAxis("Horizontal");
+         Z = Input.GetAxis("Vertical");
+
+         transform.position = transform.position + new Vector3(X, 0, Z) * Time.deltaTime  * speed;
+        
+    }
+}
